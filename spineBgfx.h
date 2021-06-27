302859880f90d24b76fb75d66f722f1e5ef56104
@sakhapov <i.sakhapov@hotmail.com>
2021-06-27 13:44:44 +0300

    small changes, remove warnings

--- spineBgfx.h
+++ spineBgfx.h
@@ -15,32 +15,37 @@
 #include "entry.h"
 #include "bgfx/platform.h"
 
+namespace extension {
+	struct VertexDecl
+	{
+		uint8_t x;
+		uint8_t y;
+		uint8_t z;
+		int16_t u;
+		int16_t v;
+		spine::Color color;
+
+		static void init()
+		{
+			ms_layout
+				.begin()
+				.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Uint8)
+				.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Int16, true, true)
+				.add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Float, true, true)
+				.end();
+		}
+
+		static bgfx::VertexLayout ms_layout;
+	};
+
+	bgfx::VertexLayout VertexDecl::ms_layout;
+}
+
 namespace spine 
 {
+
 	class SkeletonDrawable
 	{
-		struct Vertex
-		{
-			uint8_t x;
-			uint8_t y;
-			uint8_t z;
-			int16_t u;
-			int16_t v;
-			spine::Color color;
-
-			static void init()
-			{
-				ms_layout
-					.begin()
-					.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Uint8)
-					.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Int16, true, true)
-					.add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Float, true, true)
-					.end();
-			}
-
-			static bgfx::VertexLayout ms_layout;
-		};
-
 	public:
 		typedef struct Texture {
 			bgfx::TextureHandle textureHndl;
@@ -53,7 +58,7 @@ namespace spine
 		Skeleton* skeleton;
 		AnimationState* state;
 		float timeScale;
-		std::vector<Vertex>* vertexArray;
+		std::vector<extension::VertexDecl>* vertexArray;
 		VertexEffect* vertexEffect;
 
 		SkeletonDrawable(SkeletonData* skeletonData, AnimationStateData* stateData = 0);
